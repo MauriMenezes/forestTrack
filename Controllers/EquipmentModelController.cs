ec880dc7c10b0b085dfe203d18b05c539b68140e
Maurilio Souza Menezes <mauriliosm25@gmail.com>
2024-09-29 20:56:04 -0300

    feat : Add validation for existing name or empty field

--- Controllers/EquipmentModelController.cs
+++ Controllers/EquipmentModelController.cs
@@ -19,29 +19,41 @@ namespace forestTrack.Controllers
     {
         _equipmentModelRepository = equipmentModelRepository;
     }
-
+    //adicionar EquipmentModel
     [HttpPost]
     public IActionResult Add(EquipmentModel_View equipmentModel_View)
     {
-
         var equipmentModel = new EquipmentModel
         {
             Name = equipmentModel_View.Name
         };
-
         _equipmentModelRepository.Add(equipmentModel);
-
           return CreatedAtAction(nameof(Get), new { id = equipmentModel.Id }, equipmentModel);
     }
-
         [HttpGet]
         public IActionResult Get()
         {
             var equipmentModels = _equipmentModelRepository.Get();
-
             return Ok(equipmentModels);
         }
 
+        [HttpDelete]
+        public IActionResult RemoveEquipmentModel(int id)
+        {
+            try
+            {
+                _equipmentModelRepository.Remove(id);
+                return Ok(new { message = "Deletado com sucesso!" });
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound(new { message = "Equipamento não existe!" }); // Retorna 404 Not Found se não encontrar o equipamento
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); // Retorna 400 Bad Request para outros erros
+            }
+        }
     }
-    
+
 }
--- Data/repository/EquipmentModelRepository.cs
+++ Data/repository/EquipmentModelRepository.cs
@@ -12,6 +12,22 @@ namespace forestTrack.Data.repository
         private readonly ChallengeDBContext _context = new ChallengeDBContext();
         public void Add(EquipmentModel equipmentModel)
         {
+
+             //verifica nome existente ou campo vazio
+
+            if (string.IsNullOrWhiteSpace(equipmentModel.Name))
+            {
+                throw new ArgumentException("Name cannot be null or empty.", nameof(equipmentModel.Name));
+            }
+            var existingEquipment = _context.equipment_model
+                .FirstOrDefault(e => e.Name.ToLower() == equipmentModel.Name.ToLower());
+
+            if (existingEquipment != null)
+            {
+                throw new InvalidOperationException("Equipment already exists.");
+            }
+
+
            _context.equipment_model.Add(equipmentModel);
             _context.SaveChanges();
         }
@@ -23,7 +39,15 @@ namespace forestTrack.Data.repository
 
         public void Remove(int equipmentModelId)
         {
-            throw new NotImplementedException();
+            var equipmentModel = _context.equipment_model.Find(equipmentModelId);
+
+             if (equipmentModel == null)
+            {
+                throw new InvalidOperationException("Equipment not found.");
+            }
+
+            _context.equipment_model.Remove(equipmentModel);
+            _context.SaveChanges(); 
         }
     }
 }
